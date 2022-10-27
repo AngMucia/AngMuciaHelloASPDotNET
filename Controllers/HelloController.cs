2301bac242d1a1d96730c007e870150ab8d5fe90
Angel Mucia <angelmucia7@gmail.com>
2022-10-27 11:48:24 -0500

    Added views to hellocontroller

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -2,37 +2,30 @@
 
 namespace HelloASPDotNET.Controllers
 {
-    [Route("/helloworld")]
+    //[Route("/helloworld")]
     public class HelloController : Controller
     {
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            /*string html = "<form method ='post' action='/helloworld'>" +
-                "<input type='text' name='name' />"+ 
-                "<input type='submit' value='Greet Me!' />"
-                "</form>";*/
-            string html = "<form method='post' action='/helloworld>" +
-                "<input type='text' name='name' />" +
-                "<select name='language' />" +
-                "<option value='english' selected>English</option>"+
-                "<option value='french' >French</option></select>"+
-                "<input type='submit' value='Greet Me!' />"+
-                "</form>";
-                
-                
-            return Content(html, "text/html");
+            return View();
         }
 
         //[HttpGet]
         //[Route("/helloworld/welcome/{name?}")]
 
-        [HttpGet("welcome/{name?}")]
-        [HttpPost("welcome")]
+        /*[HttpGet("welcome/{name?}")]*/
+
+        [HttpPost]
+        [Route("/hello")]
         public IActionResult Welcome(string name = "World")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+            ViewBag.person = name;
+            return View();
+            
+            
+            //return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
         }
     }
 }
