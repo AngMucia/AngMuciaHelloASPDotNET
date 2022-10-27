80d88560ce9de1b31e49263e4107f34675d74fc0
Angel Mucia <angelmucia7@gmail.com>
2022-10-27 09:58:30 -0500

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNET.Controllers
+{
+    [Route("/helloworld")]
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            /*string html = "<form method ='post' action='/helloworld'>" +
+                "<input type='text' name='name' />"+ 
+                "<input type='submit' value='Greet Me!' />"
+                "</form>";*/
+            string html = "<form method='post' action='/helloworld>" +
+                "<input type='text' name='name' />" +
+                "<select name='language' />" +
+                "<option value='english' selected>English</option>"+
+                "<option value='french' >French</option></select>"+
+                "<input type='submit' value='Greet Me!' />"+
+                "</form>";
+                
+                
+            return Content(html, "text/html");
+        }
+
+        //[HttpGet]
+        //[Route("/helloworld/welcome/{name?}")]
+
+        [HttpGet("welcome/{name?}")]
+        [HttpPost("welcome")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
